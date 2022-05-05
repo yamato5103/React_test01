0b1abf9632d2993b778fd271cf99cf29d076c219
yamato5103 <sin1062.bad@gmail.com>
2022-05-06 00:03:44 +0900

    Componentize css

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,12 @@ export const App = () => {
         todoText={todoText}
         onChange={onChangeTodoText}
         onClick={onClickAdd}
+        disabled={incompleteTodos.length >= 5}
       />
+      {incompleteTodos.length >= 5 && (
+        <p style={{ color: "red" }}>登録できるTodoは5個まで</p>
+      )}
+
       <IncompleteTodos
         todos={incompleteTodos}
         onClickComplete={onClickComplete}
--- src/components/CompleteTodos.jsx
+++ src/components/CompleteTodos.jsx
@@ -1,7 +1,15 @@
+const style = {
+  backgroundColor: "#ffffe0",
+  width: "400px",
+  minHeight: "200px",
+  padding: "8px",
+  margin: "8px",
+  borderRadius: "8px"
+};
 export const CompleteTodos = (props) => {
   const { todos, onClickBack } = props;
   return (
-    <div className="complete-area">
+    <div style={style}>
       <p className="title">完了のTODO</p>
       <ul>
         {todos.map((todo, index) => {
